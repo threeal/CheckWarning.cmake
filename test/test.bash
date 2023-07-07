74343c10d70bb7bfecfd9cad4a5d402c400858cf
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-07 14:35:51 +0700

    test: handle failed test in the `test.bash` script

--- test/test.bash
+++ test/test.bash
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
 root="$(cd "$(dirname "$0")" && pwd)"
+
+let fails=0
 for dir in $root/*/ ; do
-  echo ::group::testing $(realpath --relative-to=$root $dir)...
+  name=$(realpath --relative-to=$root $dir)
+  echo ::group::testing $name...
   cmake "$dir" -B "$dir/build" -D GIT_COMMIT=$(git rev-parse --short HEAD)
+  if [ $? -ne 0 ]; then
+    echo ::error::$name test failed
+    let fails++
+  fi
   echo ::endgroup::
 done
+
+if [ $fails -gt 0 ]; then
+  exit 1
+fi
