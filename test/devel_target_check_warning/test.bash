3f9e7c4226748d30e85d172a7fd838b5b4286703
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-08 22:22:04 +0700

    test: modify test logging to be handled by each `test.bash` file

--- test/devel_target_check_warning/test.bash
+++ test/devel_target_check_warning/test.bash
@@ -2,25 +2,32 @@
 
 root=$(cd $(dirname $0) && pwd)
 
+echo ::group::Preparing for warning check testing...
 cmake $root -B $root/build
 if [ $? -ne 0 ]; then
-  echo ::error::Failed to configure the project!
+  echo ::error::Failed to configure the project.
+  echo ::endgroup::
   exit 1
 fi
+echo ::endgroup::
 
 let fails=0
 
+echo ::group::Testing warning check for success...
 cmake --build $root/build --target correct
 if [ $? -ne 0 ]; then
-  echo ::error::Build for target correct should be success!
+  echo ::error::The build for the correct target should succeed.
   let fails++
 fi
+echo ::endgroup::
 
+echo ::group::Testing warning check for failure...
 cmake --build $root/build --target incorrect
 if [ $? -eq 0 ]; then
-  echo ::error::Build for target incorrect should be failed!
+  echo ::error::The build for the incorrect target should fail.
   let fails++
 fi
+echo ::endgroup::
 
 if [ $fails -gt 0 ]; then
   exit 1
--- test/test_all.bash
+++ test/test_all.bash
@@ -4,15 +4,10 @@ root=$(cd $(dirname $0) && pwd)
 
 let fails=0
 for script in $(find $root -iname test.bash) ; do
-  dir=$(dirname $script)
-  name=$(realpath --relative-to=$root $dir)
-  echo ::group::testing $name...
   $script
   if [ $? -ne 0 ]; then
-    echo ::error::$name test failed
     let fails++
   fi
-  echo ::endgroup::
 done
 
 if [ $fails -gt 0 ]; then
