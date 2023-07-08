e342bd0f2f001132c71224fb6fae47dcaebe1c95
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-07-08 23:31:57 +0700

    style: guard string in bash with single quotation marks

--- test/devel_target_check_warning/test.bash
+++ test/devel_target_check_warning/test.bash
@@ -2,40 +2,40 @@
 
 root=$(cd $(dirname $0) && pwd)
 
-echo ::group::Preparing for warning check testing...
+echo '::group::Preparing for warning check testing...'
 cmake $root -B $root/build
 if [ $? -ne 0 ]; then
-  echo ::error::Failed to configure the project.
-  echo ::endgroup::
+  echo '::error::Failed to configure the project.'
+  echo '::endgroup::'
   exit 1
 fi
-echo ::endgroup::
+echo '::endgroup::'
 
 let fails=0
 
-echo ::group::Testing warning check for success...
+echo '::group::Testing warning check for success...'
 cmake --build $root/build --target correct
 if [ $? -ne 0 ]; then
-  echo ::error::The build for the correct target should succeed.
+  echo '::error::The build for the correct target should succeed.'
   let fails++
 fi
 echo ::endgroup::
 
-echo ::group::Testing warning check for failure...
+echo '::group::Testing warning check for failure...'
 cmake --build $root/build --target incorrect
 if [ $? -eq 0 ]; then
-  echo ::error::The build for the incorrect target should fail.
+  echo '::error::The build for the incorrect target should fail.'
   let fails++
 fi
-echo ::endgroup::
+echo '::endgroup::'
 
-echo ::group::Testing warning check with additional parameters...
+echo '::group::Testing warning check with additional parameters...'
 cmake --build $root/build --target with_parameters
 if [ $? -ne 0 ]; then
-  echo ::error::The build for the with_parameters target should fail.
+  echo '::error::The build for the with_parameters target should fail.'
   let fails++
 fi
-echo ::endgroup::
+echo '::endgroup::'
 
 if [ $fails -gt 0 ]; then
   exit 1
