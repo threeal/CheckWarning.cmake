b90ff0e843d47a5ddf50a31892f92e611d4251d6
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-17 11:51:54 +0700

    feat: merge `MSVC_FLAGS` into `FLAGS` in `target_check_warning` function

--- cmake/target_check_warning.cmake
+++ cmake/target_check_warning.cmake
@@ -1,5 +1,5 @@
 function(target_check_warning TARGET)
-  cmake_parse_arguments(ARG "" "" "FLAGS;MSVC_FLAGS" ${ARGN})
+  cmake_parse_arguments(ARG "" "" "FLAGS" ${ARGN})
 
   # Determine if the target is an interface library or not
   get_target_property(TARGET_TYPE ${TARGET} TYPE)
@@ -11,7 +11,7 @@ function(target_check_warning TARGET)
 
   # Append warning flags to the compile options
   if(MSVC)
-    target_compile_options(${TARGET} ${TYPE} /WX /permissive- /W4 /EHsc ${ARG_MSVC_FLAGS})
+    target_compile_options(${TARGET} ${TYPE} /WX /permissive- /W4 /EHsc ${ARG_FLAGS})
   else()
     target_compile_options(${TARGET} ${TYPE} -Werror -Wall -Wextra -Wpedantic ${ARG_FLAGS})
   endif()
