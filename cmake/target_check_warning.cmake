4b68d63ef48adcecf870346dc244149aff45fc8b
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-17 22:29:09 +0700

    feat: remove `FLAGS` argument in `target_check_warning` function

--- cmake/target_check_warning.cmake
+++ cmake/target_check_warning.cmake
@@ -1,6 +1,4 @@
 function(target_check_warning TARGET)
-  cmake_parse_arguments(ARG "" "" "FLAGS" ${ARGN})
-
   # Determine if the target is an interface library or not
   get_target_property(TARGET_TYPE ${TARGET} TYPE)
   if(${TARGET_TYPE} STREQUAL INTERFACE_LIBRARY)
@@ -11,8 +9,8 @@ function(target_check_warning TARGET)
 
   # Append warning flags to the compile options
   if(MSVC)
-    target_compile_options(${TARGET} ${TYPE} /WX /permissive- /W4 /EHsc ${ARG_FLAGS})
+    target_compile_options(${TARGET} ${TYPE} /WX /permissive- /W4 /EHsc)
   else()
-    target_compile_options(${TARGET} ${TYPE} -Werror -Wall -Wextra -Wpedantic ${ARG_FLAGS})
+    target_compile_options(${TARGET} ${TYPE} -Werror -Wall -Wextra -Wpedantic)
   endif()
 endfunction()
