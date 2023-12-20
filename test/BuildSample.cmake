1d9e90e9f1cf8948b8396236a69c915b7bf7269c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-20 21:07:59 +0700

    test: run different config for sample project instead of build different targets

--- test/BuildSample.cmake
+++ test/BuildSample.cmake
@@ -1,19 +1,24 @@
-message(STATUS "Configuring sample project")
 set(SAMPLE_DIR ${CMAKE_CURRENT_LIST_DIR}/sample)
+
+message(STATUS "Configuring sample project")
+list(APPEND CONFIGURE_ARGS -D CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH})
+if(WITH_UNUSED)
+  list(APPEND CONFIGURE_ARGS -D WITH_UNUSED=${WITH_UNUSED})
+endif()
+if(IGNORE_UNUSED)
+  list(APPEND CONFIGURE_ARGS -D IGNORE_UNUSED=${IGNORE_UNUSED})
+endif()
 execute_process(
-  COMMAND cmake ${SAMPLE_DIR} -B ${SAMPLE_DIR}/build -D CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
+  COMMAND cmake ${SAMPLE_DIR} -B ${SAMPLE_DIR}/build ${CONFIGURE_ARGS}
   RESULT_VARIABLE RES
 )
 if(NOT ${RES} EQUAL 0)
   message(FATAL_ERROR "Failed to configure sample project")
 endif()
 
-message(STATUS "Building project")
-foreach(TARGET ${TARGETS})
-  list(APPEND BUILD_ARGS --target ${TARGET})
-endforeach()
+message(STATUS "Building sample project")
 execute_process(
-  COMMAND cmake --build ${SAMPLE_DIR}/build ${BUILD_ARGS}
+  COMMAND cmake --build ${SAMPLE_DIR}/build
   RESULT_VARIABLE RES
 )
 if(BUILD_SHOULD_FAIL)
