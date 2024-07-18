5f97baee7416b35de4817376d46286704f222be4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-18 14:26:09 +0700

    chore: bump Assertion.cmake to version 1.0.0 (#119)

--- test/CheckWarningTest.cmake
+++ test/CheckWarningTest.cmake
@@ -1,9 +1,9 @@
 cmake_minimum_required(VERSION 3.5)
 
 file(
-  DOWNLOAD https://github.com/threeal/assertion-cmake/releases/download/v0.3.0/Assertion.cmake
+  DOWNLOAD https://github.com/threeal/assertion-cmake/releases/download/v1.0.0/Assertion.cmake
     ${CMAKE_BINARY_DIR}/Assertion.cmake
-  EXPECTED_MD5 851f49c10934d715df5d0b59c8b8c72a)
+  EXPECTED_MD5 1d8ec589d6cc15772581bf77eb3873ff)
 include(${CMAKE_BINARY_DIR}/Assertion.cmake)
 
 function(reconfigure_sample)
