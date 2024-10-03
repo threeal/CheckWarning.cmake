e24a2e566448774c12f437515fd1cd761cc46a1c
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-03 20:08:26 +0700

    docs: update function documentation in `CheckWarning.cmake` module
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- cmake/CheckWarning.cmake
+++ cmake/CheckWarning.cmake
@@ -20,23 +20,41 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Function to get warning flags based on the compiler type.
-# Arguments:
-#   - VAR: The variable for which to store the warning flags.
-function(get_warning_flags VAR)
+# Retrieves warning flags based on the current compiler.
+#
+# get_warning_flags(<output_var>)
+#
+# This function retrieves the warning flags for a specific compiler and saves
+# them to a variable named `<output_var>`. The function determines the current
+# compiler using the `CMAKE_<LANG>_COMPILER_ID` variable and retrieves the
+# warning flags for that compiler, as specified in the following table:
+#
+# | Compiler     | Warning Flags                      |
+# | ------------ | ---------------------------------- |
+# | MSVC         | `/WX /permissive- /W4 /EHsc`       |
+# | GNU or Clang | `-Werror -Wall -Wextra -Wpedantic` |
+#
+# For compilers not specified in the table above, this function will send a
+# fatal error message explaining that it does not support that compiler.
+function(get_warning_flags OUTPUT_VAR)
   if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    set("${VAR}" /WX /permissive- /W4 /EHsc PARENT_SCOPE)
+    set("${OUTPUT_VAR}" /WX /permissive- /W4 /EHsc PARENT_SCOPE)
   elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-    set("${VAR}" -Werror -Wall -Wextra -Wpedantic PARENT_SCOPE)
+    set("${OUTPUT_VAR}" -Werror -Wall -Wextra -Wpedantic PARENT_SCOPE)
   else()
     message(FATAL_ERROR "CheckWarning: Unsupported compiler for retrieving "
       "warning flags: ${CMAKE_CXX_COMPILER_ID}")
   endif()
 endfunction()
 
-# Function to enable warning checks on a specific target.
-# Arguments:
-#   - TARGET: The target for which to enable warning checks.
+# Enables warning checks on a specific target.
+#
+# target_check_warning(<target>)
+#
+# This function enables warning checks on the `<target>` by appending warning
+# flags from the `get_warning_flags` function to the compile options of that
+# target. It is equivalent to calling the `target_compile_options` command on
+# the target using the warning flags.
 function(target_check_warning TARGET)
   # Determine if the target is an interface library or not.
   get_target_property(TARGET_TYPE "${TARGET}" TYPE)
@@ -51,7 +69,14 @@ function(target_check_warning TARGET)
   target_compile_options("${TARGET}" "${TYPE}" ${FLAGS})
 endfunction()
 
-# Function to globally enable warning checks for all targets in the directory.
+# Enables warning checks on all targets in the current directory.
+#
+# add_check_warning()
+#
+# This function enables warning checks on all targets in the current directory
+# by appending warning flags from the `get_warning_flags` function to the
+# default compile options. It is equivalent to calling the `add_compile_options`
+# command using the warning flags.
 function(add_check_warning)
   get_warning_flags(FLAGS)
   add_compile_options(${FLAGS})
