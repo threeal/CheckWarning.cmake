69c124dd500f54e36a5aa3e9aca6598a7f66b8cc
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-07 21:02:36 +0700

    test: fix expected warning flags in `test_get_warning_flags.cmake`
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- test/test_get_warning_flags.cmake
+++ test/test_get_warning_flags.cmake
@@ -20,13 +20,15 @@ section("retrieve warning flags for MSVC with Clang")
 
   section("it should retrieve warning flags")
     get_warning_flags(FLAGS)
-    assert("${FLAGS}" STREQUAL "-Wall;-Wextra;-Wpedantic")
+    assert("${FLAGS}" STREQUAL "/permissive-;/W4;/EHsc")
   endsection()
 
   section("it should retrieve warning flags when treating warnings as errors")
     get_warning_flags(FLAGS TREAT_WARNINGS_AS_ERRORS)
-    assert("${FLAGS}" STREQUAL "-Wall;-Wextra;-Wpedantic;-Werror")
+    assert("${FLAGS}" STREQUAL "/permissive-;/W4;/EHsc;/WX")
   endsection()
+
+  unset(CMAKE_CXX_SIMULATE_ID)
 endsection()
 
 section("retrieve warning flags for GNU")
