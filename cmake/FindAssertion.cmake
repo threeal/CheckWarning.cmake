9ca8b3600e89829ff87b7ebe26fe9bc881730d9b
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-10-24 09:39:19 +0700

    chore: bump Assertion.cmake to version 2.0.0 (#173)
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- cmake/FindAssertion.cmake
+++ cmake/FindAssertion.cmake
@@ -1,8 +1,11 @@
 file(
-  DOWNLOAD https://github.com/threeal/assertion-cmake/releases/download/v1.0.0/Assertion.cmake
+  DOWNLOAD https://github.com/threeal/assertion-cmake/releases/download/v2.0.0/Assertion.cmake
     ${CMAKE_BINARY_DIR}/cmake/Assertion.cmake
-  EXPECTED_MD5 1d8ec589d6cc15772581bf77eb3873ff)
+  EXPECTED_MD5 5ebe475aee6fc5660633152f815ce9f6)
 include(${CMAKE_BINARY_DIR}/cmake/Assertion.cmake)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Assertion REQUIRED_VARS ASSERTION_LIST_FILE)
+find_package_handle_standard_args(
+  Assertion REQUIRED_VARS ASSERTION_VERSION VERSION_VAR ASSERTION_VERSION)
+
+list(PREPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR}/cmake)
